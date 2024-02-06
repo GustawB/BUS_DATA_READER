55f5c593e09b679640663a7c3e7efb4793f58937
GustawB <93836926+GustawB@users.noreply.github.com>
2024-02-06 13:38:04 +0100

    Init commit.

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+from data_reader import data_reader
+
+if __name__ == '__main__':
+    dt = data_reader(1, 1, 'afd497b5-83e7-4ecf-8c98-cd1805aa16c9')
+    dt.get_bus_data()
+    print(dt.read_data)
